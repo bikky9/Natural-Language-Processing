dd9a45d63c5dd6f311b4de4b5864d05f02b98b88
DrNotorious <dileep01000@gmail.com>
2020-08-31 13:43:55 +0530

    emission & transitions

--- HMM/HMM.py
+++ HMM/HMM.py
@@ -1,113 +1,49 @@
-import numpy as np
 import nltk
 import random
-from collections import Counter, defaultdict
-from sklearn.model_selection import KFold
-from statistics import mean
-import time
-import tqdm
 
+nltk.download('brown') #Brown cropus
+nltk.download('universal_tagset')  #tag set
 
-def Viterbi(train_data, test_data):
-    tagged_train_words = []
-    tagged_test_words = []
+corpus_data = list(nltk.corpus.brown.tagged_sents(tagset='universal'))
+random.shuffle(corpus_data)
+total = len(corpus_data)
 
-    for sentence in train_data:
-        for wordtuple in sentence:
-            tagged_train_words.append(wordtuple)
+train_data , test_data = corpus_data[:int(4/5*total)] , corpus_data[int(4/5*total):]
 
-    for sentence in test_data:
-        for wordtuple in sentence:
-            tagged_test_words.append(wordtuple)
+tagged_train_words = []
+tagged_test_words = []
 
-    alltags = set()
-    allwords = set()
-    for word, tag in tagged_train_words:
-        allwords.add(word)
-        alltags.add(tag)
+for sentence in train_data:
+    for wordtuple in sentence:
+        tagged_train_words.append(wordtuple)
 
-    TAGS = list(alltags)
-    WORDS = list(allwords)
-    TAGS_COUNT = len(TAGS)
+for sentence in test_data:
+    for wordtuple in sentence:
+        tagged_test_words.append(wordtuple)
 
-    TAG_OCCURENCES = Counter()
-    for word, tag in tagged_train_words:
-        TAG_OCCURENCES[tag] += 1
+alltags = set()
+allwords = set()
+for word,tag in tagged_train_words:
+    allwords.add(word)
+    alltags.add(tag)
 
-    wordEmissionProb = defaultdict(lambda: [0 for i in range(TAGS_COUNT)])
+TAGS_COUNT = len(alltags)
+WORDS_COUNT = len(allwords)
 
-    def computeAllEmissionProb():
-        # WordTagCounts = Counter()
-        # wordOccurancesCount = sum([(word == _word and tag == _tag) for _word,_tag in tagged_train_words])
-        for _word, _tag in tagged_train_words:
-            wordEmissionProb[_word][TAGS.index(_tag)] += 1
+def computeEmissionProb(word,tag):
+    words_with_tag = [_word for _word,_tag in tagged_train_words if(_tag == tag)]
+    tagOccurencesCount = len(words_with_tag)
+    wordOccurancesCount = sum([(word == _word) for _word in words_with_tag])
 
-        # for i in range(TAGS_COUNT):
-        #     tag = TAGS[i]
-        #     l.append(WordTagCounts[tag]/TAG_OCCURENCES[tag])
-        for word, v in wordEmissionProb.items():
-            l = []
-            for i in range(TAGS_COUNT):
-                l.append(v[i] / TAG_OCCURENCES[TAGS[i]])
-            wordEmissionProb[word] = l
-        print("Computing Emissions Done")
+    return (wordOccurancesCount/tagOccurencesCount)
 
-    def computeTransitionProb(tag2, tag1):
-        tags = [_tag for _word, _tag in tagged_train_words]
-        tag1Ocuurences = TAG_OCCURENCES[tag1]
-        transitions = 0
-        for i in range(len(tags) - 1):
-            if tags[i] == tag1 and tags[i + 1] == tag2:
-                transitions += 1
-        return transitions / tag1Ocuurences
+def computeTransitionProb(tag2,tag1):
+    tags = [_tag for _word,_tag in tagged_train_words]
+    tag1Ocuurences = sum([(_tag == tag1) for _tag in tags])
+    transitions = 0
+    for i in range(len(tags)-1):
+        if tags[i] == tag1 and tags[i+1] == tag2:
+            transitions += 1
+    return transitions/tag1Ocuurences
 
-    transition_matrix = np.zeros((TAGS_COUNT, TAGS_COUNT), dtype='float32')
-    for i in range(TAGS_COUNT):
-        for j in range(TAGS_COUNT):
-            transition_matrix[i, j] = computeTransitionProb(TAGS[i], TAGS[j])
-
-    def viterbi(words):
-        tags_observed = []
-        computeAllEmissionProb()
-        time.sleep(1)
-        for word in tqdm.tqdm(words):
-            probabilites = []
-            prev_tag_index = TAGS.index(tags_observed[-1]) if tags_observed else TAGS.index('.')
-            emissionProbs = wordEmissionProb[word]
-
-            for i in range(TAGS_COUNT):
-                transitionProb = transition_matrix[i, prev_tag_index]
-                emissionProb = emissionProbs[i]
-                probabilites.append(emissionProb * transitionProb)
-
-            newtag_index = probabilites.index(max(probabilites))
-            tags_observed.append(TAGS[newtag_index])
-        return list(zip(words, tags_observed))
-
-    test_words = [word for sent in test_data for word, tag in sent]
-    tagged_seq = viterbi(test_words)
-
-    check = sum([(i == j) for i, j in zip(tagged_seq, tagged_test_words)])
-    accuracy = check / len(tagged_seq)
-    print('Viterbi Algorithm Accuracy: ', accuracy * 100)
-    return accuracy
-
-
-if __name__ == '__main__':
-    nltk.download('brown')  # Brown corpus
-    nltk.download('universal_tagset')  # tag set
-
-    corpus_data = np.array(nltk.corpus.brown.tagged_sents(tagset='universal'), dtype=object)
-    random.shuffle(corpus_data)
-
-    # total = len(corpus_data)
-    #
-    # train_data, test_data = corpus_data[:int(4 / 5 * total)], corpus_data[int(4 / 5 * total):]
-
-    cv = KFold(n_splits=5)
-    accuracies = list()
-    for train_index, test_index in cv.split(corpus_data):
-        train_data, test_data = corpus_data[train_index], corpus_data[test_index]
-        accuracies.append(Viterbi(train_data, test_data))
-
-    print("Mean Accuracy after 5-fold cross Validation: ", mean(accuracies) * 100)
+print(TAGS_COUNT,WORDS_COUNT)
