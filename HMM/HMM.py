e7ec561830e75348d9d816efb2b2b80600485a98
DrNotorious <dileep01000@gmail.com>
2020-08-31 14:44:27 +0530

    Viterbi

--- HMM/HMM.py
+++ HMM/HMM.py
@@ -1,5 +1,8 @@
 import nltk
 import random
+import numpy as np
+from collections import Counter , defaultdict
+import tqdm
 
 nltk.download('brown') #Brown cropus
 nltk.download('universal_tagset')  #tag set
@@ -27,23 +30,75 @@ for word,tag in tagged_train_words:
     allwords.add(word)
     alltags.add(tag)
 
-TAGS_COUNT = len(alltags)
-WORDS_COUNT = len(allwords)
+TAGS = list(alltags)
+WORDS = list(allwords)
+TAGS_COUNT = len(TAGS)
+WORDS_COUNT = len(WORDS)
 
-def computeEmissionProb(word,tag):
-    words_with_tag = [_word for _word,_tag in tagged_train_words if(_tag == tag)]
-    tagOccurencesCount = len(words_with_tag)
-    wordOccurancesCount = sum([(word == _word) for _word in words_with_tag])
+print(TAGS_COUNT,WORDS_COUNT)
 
-    return (wordOccurancesCount/tagOccurencesCount)
+TAG_OCCURENCES = Counter()
+for word,tag in tagged_train_words:
+    TAG_OCCURENCES[tag] += 1
+
+wordEmissionProb = defaultdict(lambda  : [0 for i in range(TAGS_COUNT)])
+
+def computeAllEmissionProb():
+    # WordTagCounts = Counter()
+    # wordOccurancesCount = sum([(word == _word and tag == _tag) for _word,_tag in tagged_train_words])
+    for _word , _tag in tagged_train_words:
+        wordEmissionProb[_word][TAGS.index(_tag)] += 1
+
+    
+    # for i in range(TAGS_COUNT):
+    #     tag = TAGS[i]
+    #     l.append(WordTagCounts[tag]/TAG_OCCURENCES[tag])
+    for word,v in wordEmissionProb.items():
+        l = []
+        for i in range(TAGS_COUNT):
+            l.append(v[i]/TAG_OCCURENCES[TAGS[i]])
+        wordEmissionProb[word] = l
+    print("Computing Emissions Done")
+
+
 
 def computeTransitionProb(tag2,tag1):
     tags = [_tag for _word,_tag in tagged_train_words]
-    tag1Ocuurences = sum([(_tag == tag1) for _tag in tags])
+    tag1Ocuurences = TAG_OCCURENCES[tag1]
     transitions = 0
     for i in range(len(tags)-1):
         if tags[i] == tag1 and tags[i+1] == tag2:
             transitions += 1
     return transitions/tag1Ocuurences
 
-print(TAGS_COUNT,WORDS_COUNT)
+transition_matrix = np.zeros((TAGS_COUNT,TAGS_COUNT) , dtype= 'float32')
+for i in range(TAGS_COUNT):
+    for j in range(TAGS_COUNT):
+        transition_matrix[i,j] = computeTransitionProb(TAGS[i],TAGS[j])
+
+
+def viterbi(words):
+    tags_observed = []
+    computeAllEmissionProb()
+    for word in tqdm.tqdm(words):
+        probabilites = []
+        prev_tag_index = TAGS.index(tags_observed[-1]) if tags_observed else TAGS.index('.')
+        emissionProbs = wordEmissionProb[word]
+
+        for i in range(TAGS_COUNT):
+            transitionProb = transition_matrix[i,prev_tag_index]
+            emissionProb = emissionProbs[i]
+            probabilites.append(emissionProb*transitionProb)
+        
+        newtag_index = probabilites.index(max(probabilites))
+        tags_observed.append(TAGS[newtag_index])
+    return list(zip(words,tags_observed))
+
+
+
+test_words = [word for sent in test_data for word,tag in sent]
+tagged_seq = viterbi(test_words)
+
+check = sum([(i ==j) for i, j in zip(tagged_seq, tagged_test_words)]) 
+accuracy = check/len(tagged_seq)
+print('Viterbi Algorithm Accuracy: ',accuracy*100)
