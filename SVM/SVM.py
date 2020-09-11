eb20251d2ec0dc0b0613cbdafc3af94ee9cf9a20
Nikhil <bandinikhil007@gmail.com>
2020-09-11 19:21:29 +0530

    test

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -7,60 +7,71 @@ from sklearn.model_selection import KFold
 from statistics import mean
 from sklearn import svm
 
+
+# creating data set
 def createDataSet(training_data, testing_data):
     stemmer = SnowballStemmer("english")
-    SUFFIX = set()
-    TAGS = set()
+    featureDict = defaultdict(lambda: 0)
     for sentence in training_data:
-        for word, tag in sentence:
-            TAGS.add(tag)
+        for word, _ in sentence:
             stem = stemmer.stem(word)
             index = word.find(stem)
             if index != -1:
-                SUFFIX.add(word[index + len(stem):])
+                featureDict[word[:index]] = 0
+                featureDict[word[index + len(stem):]] = 0
+
     for sentence in testing_data:
-        for word, tag in sentence:
-            TAGS.add(tag)
+        for word, _ in sentence:
             stem = stemmer.stem(word)
             index = word.find(stem)
             if index != -1:
-                SUFFIX.add(word[index + len(stem):])
-    
-    print("feature dictionary size: " + str(len(SUFFIX)))
-    TAGS , SUFFIX = list(TAGS) , list(SUFFIX)
-    SUFFIX_SIZE = len(SUFFIX) 
-    WORD_FEATURES_SIZE = 0
-    FEATURES_SIZE = SUFFIX_SIZE + WORD_FEATURES_SIZE
-    X_train , Y_train = [] , []
+                featureDict[word[:index]] = 0
+                featureDict[word[index + len(stem):]] = 0
+
+    print("feature dictionary size: " + str(len(featureDict)))
+
+    X_train = []
+    Y_train = []
+    wordDict = defaultdict()
     for sentence in training_data:
-        prev = [0]*(FEATURES_SIZE) 
         for word, tag in sentence:
-            stem = stemmer.stem(word)
-            index = word.find(stem)
-            curr = [0]*(SUFFIX_SIZE)  #suffix features
-            if index != -1:
-                st = word[index + len(stem):]
-                curr[SUFFIX.index(st)] = 1
-            X_train.append(np.array(curr+prev))
-            Y_train.append(TAGS.index(tag))
-            prev = curr
+            if wordDict.get(word) is None:
+                x_dict = featureDict.copy()
+                stem = stemmer.stem(word)
+                index = word.find(stem)
+                if index != -1:
+                    x_dict[word[:index]] = 1
+                    x_dict[word[index + len(stem):]] = 1
+                x = np.array(list(x_dict.values()))
+                wordDict[word] = x
+                X_train.append(x)
+                Y_train.append(tag)
+            else:
+                x = wordDict[word]
+                X_train.append(x)
+                Y_train.append(tag)
 
     print("training data size: " + str(len(X_train)) + " " + str(len(Y_train)))
 
     X_test = []
     Y_test = []
     for sentence in testing_data:
-        prev , curr = [0]*(FEATURES_SIZE) , None
-        for word, tag in sentence[1:]:
-            curr = [0]*(SUFFIX_SIZE)
-            stem = stemmer.stem(word)
-            index = word.find(stem)
-            if index != -1:
-                st = word[index + len(stem):]
-                curr[SUFFIX.index(st)] = 1
-            X_test.append(np.array( curr + prev))
-            Y_test.append(TAGS.index(tag))
-            prev = curr
+        for word, tag in sentence:
+            if wordDict.get(word) is None:
+                x_dict = featureDict.copy()
+                stem = stemmer.stem(word)
+                index = word.find(stem)
+                if index != -1:
+                    x_dict[word[:index]] = 1
+                    x_dict[word[index + len(stem):]] = 1
+                x = np.array(list(x_dict.values()))
+                wordDict[word] = x
+                X_test.append(x)
+                Y_test.append(tag)
+            else:
+                x = wordDict[word]
+                X_test.append(x)
+                Y_test.append(tag)
 
     print("testing data size: " + str(len(X_test)) + " " + str(len(Y_test)))
     return (np.array(X_train), np.array(Y_train)), \
@@ -71,21 +82,18 @@ if __name__ == '__main__':
     nltk.download('brown')  # Brown corpus
     nltk.download('universal_tagset')  # tag set
 
-    corpusData = np.array(nltk.corpus.brown.tagged_sents(tagset='universal'))
-    
+    corpusData = np.array(nltk.corpus.brown.tagged_sents(tagset='universal'),
+                          dtype=object)
     random.shuffle(corpusData)
-    length = len(corpusData)
-    corpusData = corpusData[:length//5]
+
     cv = KFold(n_splits=5)
     accuracies = list()
     for train_index, test_index in cv.split(corpusData):
-        train_data, test_data = createDataSet(corpusData[train_index],corpusData[test_index])
-        clf = svm.LinearSVC(verbose = 1)
+        train_data, test_data = createDataSet(corpusData[train_index],
+                                              corpusData[test_index])
+        clf = svm.LinearSVC()
         clf.fit(train_data[0], train_data[1])
-        print("Training done")
-        X_test, Y_test = test_data
-        accuracy = clf.score(X_test, Y_test)
-        print("accuracy: " + str(accuracy))
+        accuracy = clf.score(test_data[0], test_data[1])
         accuracies.append(accuracy)
-        
+
     print("Mean Accuracy after 5-fold cross Validation: ", mean(accuracies) * 100)
