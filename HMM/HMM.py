c444370b431e5fd9e178e2d5aa7cf9cba2b8eca9
Nikhil <bandinikhil007@gmail.com>
2020-09-18 13:13:24 +0530

    Confusion Matrix

--- HMM/HMM.py
+++ HMM/HMM.py
@@ -1,49 +1,137 @@
 import nltk
+import tqdm
+import time
 import random
+import numpy as np
+import pandas as pd
+from statistics import mean
+from collections import Counter, defaultdict
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
 
-nltk.download('brown') #Brown cropus
-nltk.download('universal_tagset')  #tag set
 
-corpus_data = list(nltk.corpus.brown.tagged_sents(tagset='universal'))
-random.shuffle(corpus_data)
-total = len(corpus_data)
+def Viterbi(train_data, test_data):
+    tagged_train_words = []
+    tagged_test_words = []
 
-train_data , test_data = corpus_data[:int(4/5*total)] , corpus_data[int(4/5*total):]
+    for sentence in train_data:
+        tagged_train_words.append(('^', '^'))
+        for wordtuple in sentence:
+            tagged_train_words.append(wordtuple)
 
-tagged_train_words = []
-tagged_test_words = []
+    for sentence in test_data:
+        tagged_test_words.append(('^', '^'))
+        for wordtuple in sentence:
+            tagged_test_words.append(wordtuple)
 
-for sentence in train_data:
-    for wordtuple in sentence:
-        tagged_train_words.append(wordtuple)
+    alltags = set()
+    allwords = set()
+    for word, tag in tagged_train_words:
+        allwords.add(word)
+        alltags.add(tag)
 
-for sentence in test_data:
-    for wordtuple in sentence:
-        tagged_test_words.append(wordtuple)
+    TAGS = list(alltags)
+    WORDS = list(allwords)
+    TAGS_COUNT = len(TAGS)
 
-alltags = set()
-allwords = set()
-for word,tag in tagged_train_words:
-    allwords.add(word)
-    alltags.add(tag)
+    TAG_OCCURENCES = Counter()
+    for word, tag in tagged_train_words:
+        TAG_OCCURENCES[tag] += 1
 
-TAGS_COUNT = len(alltags)
-WORDS_COUNT = len(allwords)
+    wordEmissionProb = defaultdict(lambda: [0 for i in range(TAGS_COUNT)])
 
-def computeEmissionProb(word,tag):
-    words_with_tag = [_word for _word,_tag in tagged_train_words if(_tag == tag)]
-    tagOccurencesCount = len(words_with_tag)
-    wordOccurancesCount = sum([(word == _word) for _word in words_with_tag])
+    def computeAllEmissionProb():
+        for _word, _tag in tagged_train_words:
+            wordEmissionProb[_word][TAGS.index(_tag)] += 1
 
-    return (wordOccurancesCount/tagOccurencesCount)
+        for word, v in wordEmissionProb.items():
+            l = []
+            for i in range(TAGS_COUNT):
+                l.append(v[i] / TAG_OCCURENCES[TAGS[i]])
+            wordEmissionProb[word] = l
+        print("Computing Emissions Done")
 
-def computeTransitionProb(tag2,tag1):
-    tags = [_tag for _word,_tag in tagged_train_words]
-    tag1Ocuurences = sum([(_tag == tag1) for _tag in tags])
-    transitions = 0
-    for i in range(len(tags)-1):
-        if tags[i] == tag1 and tags[i+1] == tag2:
-            transitions += 1
-    return transitions/tag1Ocuurences
+    def computeTransitionProb(tag2, tag1):
+        tags = [_tag for _word, _tag in tagged_train_words]
+        tag1Ocuurences = TAG_OCCURENCES[tag1]
+        transitions = 0
+        for i in range(len(tags) - 1):
+            if tags[i] == tag1 and tags[i + 1] == tag2:
+                transitions += 1
+        return transitions / tag1Ocuurences
 
-print(TAGS_COUNT,WORDS_COUNT)
+    transition_matrix = np.zeros((TAGS_COUNT, TAGS_COUNT), dtype='float32')
+    for i in range(TAGS_COUNT):
+        for j in range(TAGS_COUNT):
+            transition_matrix[i, j] = computeTransitionProb(TAGS[i], TAGS[j])
+
+    def unknown(word):
+        if word.endswith('ing') or word.endswith('ed'):
+            return "VERB"
+        if word.endswith('s') or not word.islower():
+            return "NOUN"
+        else:
+            return "X"
+
+    def viterbi(words):
+        tags_observed = []
+        computeAllEmissionProb()
+        time.sleep(1)
+        for word in tqdm.tqdm(words):
+            probabilites = []
+            prev_tag_index = TAGS.index(tags_observed[-1]) if tags_observed else TAGS.index('.')
+            emissionProbs = wordEmissionProb[word]
+
+            for i in range(TAGS_COUNT):
+                transitionProb = transition_matrix[i, prev_tag_index]
+                emissionProb = emissionProbs[i]
+                probabilites.append(emissionProb * transitionProb)
+
+            newtag_index = probabilites.index(max(probabilites))
+
+            tags_observed.append(TAGS[newtag_index] if probabilites[newtag_index] else unknown(word))
+        return list(zip(words, tags_observed))
+
+    test_words = [word for word, tag in tagged_test_words]
+    tagged_seq = viterbi(test_words)
+
+    check = sum([(i == j) for i, j in zip(tagged_seq, tagged_test_words)])
+    accuracy = check / len(tagged_seq)
+    Y_pred = [tag for word, tag in tagged_seq]
+    Y_test = [tag for word, tag in tagged_test_words]
+
+    # (un)comment lines till line 116, not to display confusion matrix and per POS accuracy
+    # confusionMatrix = confusion_matrix(Y_test, Y_pred, labels=TAGS)
+    #
+    # for i in range(confusionMatrix.shape[0]):
+    #     print(TAGS[i], " accuracy: ", (confusionMatrix[i][i] / sum(confusionMatrix[i])) * 100)
+    # pd.set_option('display.max_rows', None)
+    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.width', None)
+    # pd.set_option('display.max_colwidth', None)
+    # df = pd.DataFrame(confusionMatrix)
+    # df.columns = TAGS
+    # df.index = TAGS
+    # df.style
+    # print(df)
+
+    print('Viterbi Algorithm Accuracy: ', accuracy * 100)
+    return accuracy
+
+
+if __name__ == '__main__':
+    nltk.download('brown')  # Brown corpus
+    nltk.download('universal_tagset')  # tag set
+
+    corpus_data = np.array(nltk.corpus.brown.tagged_sents(tagset='universal'), dtype=object)
+    random.shuffle(corpus_data)
+
+    cv = KFold(n_splits=5)
+    accuracies = list()
+    for train_index, test_index in cv.split(corpus_data):
+        train_data, test_data = corpus_data[train_index], corpus_data[test_index]
+        accuracy = Viterbi(train_data, test_data)
+        accuracies.append(accuracy)
+        # (un)comment this for 5-fold cross validation
+        # break
+    print("Mean Accuracy after 5-fold cross Validation: ", mean(accuracies) * 100)
