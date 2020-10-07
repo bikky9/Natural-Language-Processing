dc8803ae720d1b7329c25983ac18dac7bc8e8351
DrNotorious <dileep01000@gmail.com>
2020-10-07 15:11:37 +0530

    MEMM megam opt

--- chunking/MEMM/MEMM.py
+++ chunking/MEMM/MEMM.py
@@ -1,5 +1,9 @@
 import nltk
 from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+
+TAGS = ['B','I','O']
 
 def features(**args):
         return args
@@ -9,52 +13,71 @@ class ChunkTagger():
         train_set = []
         for sent in tqdm(TRAIN_DATA):
             sentLen = len(sent)
-            prevword , prevpos = "<NONE>" , "<NONE>"
+            prevword , prevpos , prevtag = "<NONE>" , "<NONE>" , "<NONE>"
             for i , (word,pos,tag) in enumerate(sent):
                 if(i == (sentLen-1)): nextword, nextpos = "<NULL>" , "<NULL>"
                 else: nextword, nextpos = sent[i+1][0] , sent[i+1][1]
-                feat = features(pos=pos,word=word,prevpos=prevpos,nextpos=nextpos)
+                feat = features(pos=pos,word=word,prevpos=prevpos,nextpos=nextpos,prevtag=prevtag,prevcurrpos=f"{prevpos}+{pos}",currnextpos=f"{pos}+{nextpos}")
                 train_set.append((feat,tag))
+                prevword , prevpos , prevtag = word , pos , tag
         print("Started MaxEnt Class Training")
-        self.classifier = nltk.MaxentClassifier.train(train_set,trace=4)
+        self.classifier = nltk.MaxentClassifier.train(train_set,trace=0,algorithm="megam")
         print("Classifier Trained")
     
     def tag(self,sent):
         tags = []
         sentLen = len(sent)
-        prevword , prevpos = "<NONE>" , "<NONE>"
+        prevword , prevpos , prevtag = "<NONE>" , "<NONE>" , "<NONE>"
         for i , (word,pos,tag) in enumerate(sent):
             if(i == (sentLen-1)): nextword, nextpos = "<NULL>" , "<NULL>"
             else: nextword, nextpos = sent[i+1][0] , sent[i+1][1]
-            feat = features(pos=pos,word=word,prevpos=prevpos,nextpos=nextpos)
+            feat = features(pos=pos,word=word,prevpos=prevpos,nextpos=nextpos,prevtag=prevtag,prevcurrpos=f"{prevpos}+{pos}",currnextpos=f"{pos}+{nextpos}")
             tag  = self.classifier.classify(feat)
             tags.append(tag)
+            prevword , prevpos , prevtag = word , pos , tag
         return list(zip(sent,tags))
     
-
-class ChunkParser():
-    def __init__(self,TRAIN_DATA):
-        self.tagger = ChunkTagger(TRAIN_DATA)
-
     def evaluate(self,TEST_DATA):
-        preds = [self.tagger.tag(sent) for sent in TEST_DATA]
-        correct , total = 0 , 0
+        preds = [self.tag(sent) for sent in TEST_DATA]
+        y_test , y_pred = [] , []
+        corr , total = 0, 0
         for s in preds:
             total += len(s)
             for (w,pos,t1) , t2 in s:
-                correct += (t1 == t2)
-        print("Accuracy :" , correct/total)
+                corr += (t1 == t2)
+                y_test.append(t1[0]); y_pred.append(t2)
+        confusionMatrix = confusion_matrix(y_test, y_pred, labels=TAGS)
+        # print(confusion_matrix)
+        for i in range(confusionMatrix.shape[0]):
+            print(TAGS[i], " accuracy: ", (confusionMatrix[i][i] / sum(confusionMatrix[i])) * 100)
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+        pd.set_option('display.max_colwidth', None)
+        df = pd.DataFrame(confusionMatrix)
+        df.columns = TAGS
+        df.index = TAGS
+        df.style
+        print(df)
+        print("Accuracy :" , 100 * corr / total )
+    
 
 
 if __name__ == "__main__":
 
+    nltk.download("conll2000")
+    nltk.config_megam('./megam-64.opt')
     corpus_train = nltk.corpus.conll2000.chunked_sents('train.txt')
     corpus_test = nltk.corpus.conll2000.chunked_sents("test.txt")
 
-    TRAIN_DATA = [nltk.tree2conlltags(sent) for sent in corpus_train]
-    TEST_DATA = [nltk.tree2conlltags(sent) for sent in corpus_test]
+    def preprocess(sent):
+        tgs = nltk.tree2conlltags(sent)
+        tgs = [(w,pos,t[0]) for w,pos,t in tgs]
+        return tgs
+    TRAIN_DATA = [preprocess(sent) for sent in corpus_train]
+    TEST_DATA = [preprocess(sent) for sent in corpus_test]
 
-    cP = ChunkParser(TRAIN_DATA)
+    cP = ChunkTagger(TRAIN_DATA)
     cP.evaluate(TEST_DATA)
     
     
